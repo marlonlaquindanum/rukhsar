aeeb1094ff8ad87323f7e7378fd6b434e07524d1
marlon <marlon.laquindanum06@gmail.com>
2024-05-13 14:33:11 +0000

    Update from Shopify for theme rukhsar/dev_branch
    Committed from shop: My Store

--- assets/nav-product.js
+++ assets/nav-product.js
@@ -1,4 +1,5 @@
   let navbar = document.getElementById("nav-section");
+console.log("nav-setion==>", navbar);
   let navOffset = navbar.offsetTop;
   window.addEventListener("scroll", () => {
     (window.scrollY >= navOffset) ? navbar.classList.add("fixed-nav") : navbar.classList.remove("fixed-nav")
