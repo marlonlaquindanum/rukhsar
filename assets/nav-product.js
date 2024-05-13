742c27f62ff3500304c7d885754409ca90ac190a
marlon <marlon.laquindanum06@gmail.com>
2024-05-13 15:26:08 +0000

    Update from Shopify for theme rukhsar/dev_branch
    Committed from shop: My Store

--- assets/nav-product.js
+++ assets/nav-product.js
@@ -1,8 +1,9 @@
   let navbar = document.getElementById("nav-section");
   let navOffset = navbar.offsetTop;
+  let naveOverProduct = 1200;
 
   window.addEventListener("scroll", () => {
     console.log("navbar==>", scrollY);
-    (window.scrollY > navOffset) ? navbar.classList.add("fixed-nav") : navbar.classList.remove("fixed-nav");
-    (window.scrollY > navOffset) ? navbar.classList.remove("nav-bar") : navbar.classList.add("nav-bar");
+    (window.scrollY > naveOverProduct) ? navbar.classList.add("fixed-nav") : navbar.classList.remove("fixed-nav");
+    (window.scrollY > naveOverProduct) ? navbar.classList.remove("nav-bar") : navbar.classList.add("nav-bar");
   });
